e020690931cb9a7a5c869e8f22339ccdb703ddc5
Alexander Poluektov <alexander.poluektov@gmail.com>
2011-11-13 19:42:32 +0100

    License and license statements in sources added.

--- Read.hs
+++ Read.hs
@@ -1,3 +1,8 @@
+-- Copyright (c) 2011 Alexander Poluektov (alexander.poluektov@gmail.com)
+--
+-- Use, modification and distribution are subject to the MIT license
+-- (See accompanyung file MIT-LICENSE)
+
 module Read
     (
       readHand
