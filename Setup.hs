4efbe9a108d37b33e87a7d3ad146cf711749766e
Alexander Poluektov <alexander.poluektov@gmail.com>
2011-11-14 21:22:02 +0100

    Cabalized.

---
+++ Setup.hs
@@ -0,0 +1,7 @@
+-- Copyright (c) 2011 Alexander Poluektov (alexander.poluektov@gmail.com)
+--
+-- Use, modification and distribution are subject to the MIT license
+-- (See accompanyung file MIT-LICENSE)
+
+import Distribution.Simple
+main = defaultMain
