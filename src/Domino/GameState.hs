515d01d08efd74cd0dee13f7d6e1f315a5d51d4e
Alexander Poluektov <alexander.poluektov@gmail.com>
2011-11-16 20:44:06 +0100

    updateGameState refactored out from restoreGameState as I plan to use it in more complicated game strategies.

--- src/Domino/GameState.hs
+++ src/Domino/GameState.hs
@@ -14,21 +14,20 @@ data GameState = GameState {
     , table        :: Table
     } deriving (Show)
 
+updateGameState :: (PlayerId,Event) -> GameState -> GameState
+updateGameState (_, (EBegin h _)) st = (GameState (28 - length h) 7 h Empty)
+updateGameState (Opponent, (EMove m)) (GameState stk oh h t)
+    = (GameState stk (oh-1) h (makeMove t m))
+updateGameState (Me, (EMove m@(Move p _))) (GameState stk oh h t)
+    = GameState stk oh newH (makeMove t m)
+      where newH = filter (neq p) h
+            neq (a,b) (x,y) = ((a,b) /= (x,y) && (a,b) /= (y,x))
+updateGameState (Opponent,(EDraw _)) (GameState stk oh h t)
+    = GameState (stk-1) (oh+1) h t
+updateGameState (Me, (EDraw (Known p))) (GameState stk oh h t)
+    = GameState (stk-1) oh (p:h) t
+updateGameState (_, EPass) gs = gs
+
 
 restoreGameState :: GameEvents -> GameState
-restoreGameState evts = foldr f (GameState 28 0 [] Empty) evts
-    where f :: (PlayerId,Event) -> GameState -> GameState
-          f (_, (EBegin h _)) st = (GameState (28 - length h) 7 h Empty)
-          f (Opponent, (EMove m)) (GameState stk oh h t)
-              = (GameState stk (oh-1) h (makeMove t m))
-          f (Me, (EMove m@(Move p _))) (GameState stk oh h t)
-              = GameState stk oh newH (makeMove t m)
-                where newH = filter (neq p) h
-                      neq (a,b) (x,y) = ((a,b) /= (x,y) && (a,b) /= (y,x))
-          f (Opponent,(EDraw _)) (GameState stk oh h t)
-              = GameState (stk-1) (oh+1) h t
-          f (Me, (EDraw (Known p))) (GameState stk oh h t)
-              = GameState (stk-1) oh (p:h) t
-          f (_, EPass) gs = gs
-
-
+restoreGameState evts = foldr updateGameState (GameState 28 0 [] Empty) evts
