a6daabef1547ee321c21b97627bf949f98f712c1
Alexander Poluektov <alexander.poluektov@gmail.com>
2011-11-19 20:58:37 +0100

    Minor (cleaning up of restoreOpponentHand).

--- src/Domino/Strategy/Counting.hs
+++ src/Domino/Strategy/Counting.hs
@@ -29,26 +29,27 @@ counting = (Strategy f)
                     where f (n,i) = (i == a || i == b)
                           (a,b) = ends $ makeMove (line st) m
 
-oppChoices evts m = sum $ fst $ unzip $ filter f $ zip opHand [0..6]
-    where f (n,i) = (i == a || i == b)
-          (a,b) = ends $ makeMove (line st) m
-          st = restoreGameState evts
-          opHand = restoreOpponentHand evts
-
-
 restoreOpponentHand :: GameEvents -> OpponentHand
-restoreOpponentHand evts = fst $ foldr f ((replicate 7 7), initialState) evts
+restoreOpponentHand evts = fst $ foldr f (initialOpponentHand, initialState) evts
     where f :: (Player, Event) -> (OpponentHand, GameState) -> (OpponentHand, GameState)
-          f e@(_, (EBegin h _)) (oh,gs)   = (foldr upd oh h, updateGameState e gs)
-          f e@(Opponent, (EMove (Move p _))) (oh,gs) = (upd p oh, updateGameState e gs)
+          f e@(_, (EBegin h _)) (oh,gs)
+              = (foldr updateOpponentHand oh h, updateGameState e gs)
+          f e@(Opponent, (EMove (Move p _))) (oh,gs)
+              = (updateOpponentHand p oh, updateGameState e gs)
           f e@(Opponent, (EDraw _)) (oh,gs) = (newOh, updateGameState e gs)
               where newOh = map noEnds (zip oh [0..6])
                     noEnds (n,i) | i == a || i == b = 0
                                  | otherwise        = n
                     (a,b) = ends $ line gs
-          f e@(Me, (EDraw (Known p))) (oh,gs) = (upd p oh, updateGameState e gs)
+          f e@(Me, (EDraw (Known p))) (oh,gs)
+              = (updateOpponentHand p oh, updateGameState e gs)
           f e (oh,gs) = (oh, updateGameState e gs)
-          upd :: Tile -> OpponentHand -> OpponentHand
-          upd (a,b) oh = map u (zip oh [0..6])
-              where u (n,i) | i == a || i == b = max (n-1) 0
-                            | otherwise        = n
+
+
+updateOpponentHand :: Tile -> OpponentHand -> OpponentHand
+updateOpponentHand (a,b) oh = map u (zip oh [0..6])
+    where u (n,i) | i == a || i == b = max (n-1) 0
+                  | otherwise        = n
+
+initialOpponentHand :: OpponentHand
+initialOpponentHand = (replicate 7 7)
