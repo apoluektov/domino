416279c96f8402537227f6f00a29b5b8f06dfd68
Alexander Poluektov <alexander.poluektov@gmail.com>
2011-11-29 20:44:38 +0100

    Game loop: GameState has been hidden in State monad, not passed explicitly.

--- src/Main.hs
+++ src/Main.hs
@@ -10,6 +10,8 @@ import Domino.Strategy
 import Domino.Strategy.Simple
 import Domino.Strategy.Counting
 
+import Control.Monad.State
+
 data GameResult = GRDraw | GRWin Player
                 deriving (Show, Eq)
 
@@ -24,34 +26,43 @@ game = do
   putStrLn "Whose move is the first?"
   first <- readFirst
   let e = (first, EBegin hand first)
-  loop first (notify simpleStrat e) (updateGameState e initialState)
+  evalStateT (loop first (notify counting e)) (updateGameState e initialState)
 
 
-loop :: Player -> Strategy -> GameState -> IO GameResult
-loop Opponent s st = do
-  putStrLn "What is opponent's move?"
-  move <- readMove
+type StateGameState = StateT GameState IO
+
+update :: (Player, Event) -> StateGameState ()
+update evt = do
+  modify $ updateGameState evt
+
+-- TODO: clean me
+loop :: Player -> Strategy -> StateGameState GameResult
+loop Opponent s = do
+  lift $ putStrLn "What is opponent's move?"
+  st <- get
+  move <- lift $ readMove
   case move of
-    EDraw Unknown -> loop Opponent (notify s (Opponent,move)) (updateGameState (Opponent,move) st)
+    EDraw Unknown ->  (update (Opponent,move)) >> (loop Opponent (notify s (Opponent,move)))
     EPass | head (events st) == (Me,EPass) -> return GRDraw
-          | otherwise -> loop Me (notify s (Opponent,move)) (updateGameState (Opponent,move) st)
+          | otherwise -> (update (Opponent,move)) >> (loop Me (notify s (Opponent,move)))
     EMove m | not (isCorrectMove (line st) m) -> do
-                putStrLn "Move is not correct; try again:"
-                loop Opponent s st
+                lift $ putStrLn "Move is not correct; try again:"
+                loop Opponent s
             | checkWin Opponent (updateGameState (Opponent,move) st) -> return (GRWin Opponent)
-            | otherwise -> loop Me (notify s (Opponent,move)) (updateGameState (Opponent,move) st)
-loop Me s st = do
+            | otherwise -> (update (Opponent,move)) >> loop Me (notify s (Opponent,move))
+loop Me s = do
   let evt = next s
-  putStrLn $ show evt
+  st <- get
+  lift $ putStrLn $ show evt
   case evt of
      EDraw Unknown -> do
-             putStrLn "What did I get from the stock?"
-             tile <- readTile
-             loop Me (notify s (Me,EDraw $ Known tile)) (updateGameState (Me,EDraw $ Known tile) st)
+             lift $ putStrLn "What did I get from the stock?"
+             tile <- lift $ readTile
+             (update (Me,EDraw $ Known tile)) >> loop Me (notify s (Me,EDraw $ Known tile))
      EPass | head (events st) == (Opponent,EPass) -> return GRDraw
-           | otherwise -> loop Opponent (notify s (Me,evt)) (updateGameState (Me,evt) st)
+           | otherwise -> (update (Me,evt)) >> loop Opponent (notify s (Me,evt))
      EMove m | checkWin Me (updateGameState (Me,evt) st) -> return (GRWin Me)
-             | otherwise -> loop Opponent (notify s (Me,evt)) (updateGameState (Me,evt) st)
+             | otherwise -> (update (Me,evt)) >> loop Opponent (notify s (Me,evt))
 
 checkWin :: Player -> GameState -> Bool
 checkWin p st = numTiles p st == 0
